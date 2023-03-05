62741c5686b555f235620e6887eb4bd88bd8cab6
Fawas6 <adeoyemuhdfawas@gmail.com>
2023-03-05 01:42:39 +0100

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Hernalytics/',
-    plugins: [react()],
+  plugins: [react()],
 })
